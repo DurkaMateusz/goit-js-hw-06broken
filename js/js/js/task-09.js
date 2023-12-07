d1ac69a5054ecbd9292bf4ffca329c64b59dab05
DurkaMateusz <121753983+DurkaMateusz@users.noreply.github.com>
2023-12-08 00:59:25 +0100

    09 finished
    as tittle

--- js/js/js/task-09.js
+++ js/js/js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorOfBtn = document.querySelector(".change-color");
+const colorOfSpan = document.querySelector(".color");
+
+      colorOfBtn.addEventListener("click", () => {
+        const randomColor = getRandomHexColor();
+
+        document.body.style.backgroundColor = randomColor;
+        colorOfSpan.textContent = randomColor;
+      });
